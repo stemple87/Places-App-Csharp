7c10b841023d04db1bb9a58ac17570700511f51e
Simon Temple Timothy Durward <simonctemple@live.com | timothy.a.durward@gmail.com>
2016-02-10 11:26:26 -0800

    add initital files

---
+++ Objects/madlibObject.cs
@@ -0,0 +1,13 @@
+// namespace madLibLibrary {
+
+//   public class storyVariables {
+//     public string name1 {get; set; }
+//     public string name2 {get; set; }
+//     public string creature {get; set; }
+//     public string weapon {get; set; }
+//     public string vehicle {get; set; }
+//     public string treasure {get; set; }
+//   }
+// }
+
+// Test example above
